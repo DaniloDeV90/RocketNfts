4ac8750035e4d88a3f4220084abff1ff0e2867ff
DaniloDeV90 <duhflores78@gmail.com>
2023-05-07 16:36:12 -0300

    correcoess

--- RocketNft/src/Components/Container1/Container1.jsx
+++ RocketNft/src/Components/Container1/Container1.jsx
@@ -3,6 +3,14 @@ import "./Container1.css"
 
 import grupo from "../../assets/group-avarts.png"
 import SvgRaposa from './SvgRaposa.jsx'
+import galeria1 from "../../assets/galeria-1.png"
+import galeria2 from "../../assets/galeria-2.png"
+import galeria3 from "../../assets/galeria-3.png"
+import galeria4 from "../../assets/galeria-4.png"
+import galeria5 from "../../assets/galeria-5.png"
+import galeria6 from "../../assets/galeria-6.png"
+import galeria7 from "../../assets/galeria-7.png"
+import galeria8 from "../../assets/galeria-8.png"
 import Logo from './Logo.jsx'
 
 
@@ -48,7 +56,25 @@ const Container1 = () => {
     </div>
 
     <div className="imagensAstronautas">
-<p>sjdfhjdf</p>
+
+
+
+<div className="card">
+
+<img src={galeria1} alt="" />
+
+<img src={galeria2} alt="" />
+
+<img src={galeria3} alt="" />
+
+<img src={galeria4} alt="" />
+
+
+<img src={galeria5} alt="" />
+<img src={galeria6} alt="" />
+<img src={galeria7} alt="" />
+<img src={galeria8} alt="" />
+</div>
     </div>
 </div>
 
